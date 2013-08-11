8fc5870219e9a9de6eb2fb5a3ed0dc8c8b2dde8e
Satish Krishnamurthy <sakrishnamurthy@corp.paypal.com>
2013-08-10 20:49:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'c413bdbec915667fb5e1d46cc2f0acac5a4a5cbcd7ec3a66b1bede899c323c1b08c81af62c4ebb8e332931411b96f3421f5838f85326e1084f4f0c32945a72eb'
